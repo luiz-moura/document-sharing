17deeb1e52499efeaff2019f32c9d2fbe1de5930
Luiz Moura <moura.oliveira.luiz@gmail.com>
2024-08-18 20:55:02 -0400

    feat: create dropbox record in hosting table

---
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20240819000713.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Database\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240819000713 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Insert dropbox record in hosting table';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql("INSERT INTO hosting (name, slug) VALUES ('Dropbox', 'dropbox');");
+
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql("DELETE FROM hosting WHERE slug = 'dropbox'");
+
+    }
+}
