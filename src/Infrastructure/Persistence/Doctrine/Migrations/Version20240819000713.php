ba3d79d7162df61a7f24b74fd55c564800781d06
Luiz Moura <luiz.moura@kanastra.com.br>
2024-08-27 21:44:46 -0400

    style: remove useless comments from migrations

--- src/Infrastructure/Persistence/Doctrine/Migrations/Version20240819000713.php
+++ src/Infrastructure/Persistence/Doctrine/Migrations/Version20240819000713.php
@@ -7,9 +7,6 @@ namespace Database\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20240819000713 extends AbstractMigration
 {
     public function getDescription(): string
