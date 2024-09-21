391cfa75fe7bdd0fd745ccf78fa24f61fbe3d3c7
Luiz Moura <luiz.moura@kanastra.com.br>
2024-09-21 14:41:26 -0400

    feat: create abstract job

--- src/Domain/Sender/Jobs/SendFileToHostingJob.php
+++ src/Domain/Sender/Jobs/SendFileToHostingJob.php
@@ -4,19 +4,22 @@ declare(strict_types=1);
 
 namespace App\Domain\Sender\Jobs;
 
+use App\Domain\Common\Jobs\AbstractJob;
 use App\Domain\Common\Jobs\Contracts\Job;
+use App\Domain\Common\Queue\Contracts\Publisher;
+use App\Domain\Sender\Actions\SendFileToHostingAction;
 
-class SendFileToHostingJob implements Job
+class SendFileToHostingJob extends AbstractJob implements Job
 {
-
     public function __construct(
-    ) {}
+        private Publisher $publisher,
+        private SendFileToHostingAction $sendFileToHostingAction,
+    ) {
+        parent::__construct($publisher);
+    }
 
     public function handle(): void
     {
-    }
-
-    public function dispatch(mixed ...$args): void
-    {
+        ($this->sendFileToHostingAction)(...$this->args);
     }
 }
