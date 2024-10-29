f6aba9b756ce6cdf9ce1ceaeb74435971aa06919
Luiz Moura <moura.oliveira.luiz@gmail.com>
2024-10-28 21:33:56 -0400

    feat: create dropbox token provider

---
+++ src/Infrastructure/Integrations/Hosting/Dropbox/DropboxRefreshableTokenProvider.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Infrastructure\Integrations\Hosting\Dropbox;
+
+use Fig\Http\Message\StatusCodeInterface as Status;
+use GuzzleHttp\Exception\ClientException;
+use Spatie\Dropbox\RefreshableTokenProvider;
+use Throwable;
+
+class DropboxRefreshableTokenProvider extends DropboxTokenProvider implements RefreshableTokenProvider
+{
+    public function refresh(ClientException $exception): bool
+    {
+        $this->logger->info(sprintf('[%s] Trying to refresh the token', __METHOD__));
+
+        if (Status::STATUS_UNAUTHORIZED !== $exception->getCode()) {
+            $this->logger->info(sprintf('[%s] The token was not refreshed because the error is not an auth error', __METHOD__), [
+                'error' => $exception->getMessage(),
+            ]);
+
+            return false;
+        }
+
+        try {
+            $response = $this->guzzleClient->post('oauth2/token', [
+                'form_params' => [
+                    'grant_type' => 'refresh_token',
+                    'refresh_token' => $this->refreshToken,
+                    'client_id' => $this->clientId,
+                    'client_secret' => $this->clientSecret,
+                ],
+            ]);
+
+            /**
+             * @var array {access_token: string, token_type: string, expires_in: int} $body
+             */
+            $body = json_decode($response->getBody()->getContents(), true);
+
+            $this->accessToken = $body['access_token'];
+
+            $this->logger->info(sprintf('[%s] Token has been refreshed', __METHOD__));
+
+            return true;
+        } catch (Throwable $e) {
+            $this->logger->info(sprintf('[%s] Failed to generate new token', __METHOD__), [
+                'error' => $e->getMessage(),
+            ]);
+
+            return false;
+        }
+    }
+}
---
+++ src/Infrastructure/Integrations/Hosting/Dropbox/Exceptions/AccessTokenNotDefinedException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Infrastructure\Integrations\Hosting\Dropbox\Exceptions;
+
+use Exception;
+
+class AccessTokenNotDefinedException extends Exception
+{
+    public function __construct()
+    {
+        parent::__construct('The access token is not defined');
+    }
+}
