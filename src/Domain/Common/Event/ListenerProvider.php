81d12bf8974398759e98439f96d2cece418f572f
Luiz Moura <moura.oliveira.luiz@gmail.com>
2024-11-01 21:51:58 -0400

    fix: clear event listeners

--- src/Domain/Common/Event/ListenerProvider.php
+++ src/Domain/Common/Event/ListenerProvider.php
@@ -2,16 +2,12 @@
 
 namespace App\Domain\Common\Event;
 
-use App\Domain\Sender\Events\FileSentToHostEvent;
-use App\Domain\Sender\Listeners\NotifyFileSentToHostListener;
 use Psr\EventDispatcher\ListenerProviderInterface;
 
 final class ListenerProvider implements ListenerProviderInterface
 {
     private array $listeners = [
-        FileSentToHostEvent::class => [
-            new NotifyFileSentToHostListener(),
-        ],
+        //
     ];
 
     public function getListenersForEvent(object $event): iterable
