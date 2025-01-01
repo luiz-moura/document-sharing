f6e67a6d0074399d0dcd1948ebf61cbdb3f47e60
Luiz Moura <moura.oliveira.luiz@gmail.com>
2024-12-31 20:23:09 -0400

    feat: create onlyStrings attribute validator

--- src/Domain/Common/Validators/Rules/OnlyNumbers.php
+++ src/Domain/Common/Validators/Rules/OnlyNumbers.php
@@ -9,12 +9,12 @@ use App\Domain\Common\Validators\Exceptions\ValidationException;
 use Attribute;
 
 #[Attribute(Attribute::TARGET_PROPERTY)]
-class OnlyNumbers implements Validation
+final class OnlyNumbers implements Validation
 {
     public function validate(string $propertyName, mixed $value, mixed ...$args): mixed
     {
         foreach ($value as $item) {
-            if (!is_int($item)) {
+            if (! is_int($item)) {
                 throw new ValidationException("Only integer values are allowed in the {$propertyName} field.");
             }
         }
