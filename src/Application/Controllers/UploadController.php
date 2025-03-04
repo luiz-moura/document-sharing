5114d7a5012ad1f86af8569a2f1adc52cf1949a3
Luiz Moura <moura.oliveira.luiz@gmail.com>
2025-03-04 12:37:53 -0400

    feat: validate request

--- src/Application/Controllers/UploadController.php
+++ src/Application/Controllers/UploadController.php
@@ -8,6 +8,7 @@ use App\Domain\Sender\Actions\UploadFileAction;
 use App\Domain\Sender\DTOs\UploadRequestData;
 use App\Domain\Sender\Enums\FileStatusOnHostEnum;
 use Fig\Http\Message\StatusCodeInterface as StatusCode;
+use InvalidArgumentException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -20,21 +21,42 @@ class UploadController
 
     public function upload(Request $request, Response $response): Response
     {
+        $this->validateRequest($request);
+
         $uploadRequest = new UploadRequestData(
-            hostingSlugs: $request->getParsedBody()['hosting_slugs'] ?? [],
-            uploadedFiles: $request->getUploadedFiles()['files'] ?? [],
-            shouldZip: filter_var($request->getParsedBody()['should_zip'] ?? false, FILTER_VALIDATE_BOOLEAN),
+            hostingSlugs: $request->getParsedBody()['hosting_slugs'],
+            uploadedFiles: $request->getUploadedFiles()['files'],
+            shouldZip: $request->getParsedBody()['should_zip'],
         );
 
         $fileUuid = ($this->uploadFileAction)($uploadRequest);
 
-        $response->getBody()->write(json_encode([
-            'file_id' => $fileUuid,
-            'status' => FileStatusOnHostEnum::RECEIVED->value
-        ]));
+        $response->getBody()->write(
+            json_encode([
+                'file_id' => $fileUuid,
+                'status' => FileStatusOnHostEnum::RECEIVED->value
+            ])
+        );
 
         return $response
             ->withStatus(StatusCode::STATUS_CREATED)
             ->withHeader('Content-Type', 'application/json');
+    }
+
+    private function validateRequest(Request $request): void
+    {
+        $parsedBody = $request->getParsedBody();
+
+        if (! isset($parsedBody['hosting_slugs']) || ! is_array($parsedBody['hosting_slugs'])) {
+            throw new InvalidArgumentException('hosting_slugs must be an array');
+        }
+
+        if (! isset($parsedBody['should_zip']) || ! is_bool($parsedBody['should_zip'])) {
+            throw new InvalidArgumentException('should_zip must be a boolean');
+        }
+
+        if (! isset($request->getUploadedFiles()['files'])) {
+            throw new InvalidArgumentException('files must be uploaded');
+        }
     }
 }
