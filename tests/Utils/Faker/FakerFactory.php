6789a15ae772ec149da5e0b49113913f65af1946
Luiz Moura <moura.oliveira.luiz@gmail.com>
2025-03-24 21:44:39 -0400

    fix: implicitly nullable parameters are deprecated

--- tests/Utils/Faker/FakerFactory.php
+++ tests/Utils/Faker/FakerFactory.php
@@ -7,7 +7,7 @@ namespace Tests\Utils\Faker;
 use Faker\Factory;
 use Faker\Generator;
 
-function faker(string $locale = null): Generator
+function faker(?string $locale = null): Generator
 {
     return Factory::create($locale ?? Factory::DEFAULT_LOCALE);
 }
