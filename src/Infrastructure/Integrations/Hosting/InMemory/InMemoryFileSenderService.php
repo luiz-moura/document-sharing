212526a328ba713e8f0313e59b156b9ae1712ef4
Luiz Moura <moura.oliveira.luiz@gmail.com>
2025-03-27 19:48:19 -0400

    fix: fix file sender service in memory

--- src/Infrastructure/Integrations/Hosting/InMemory/InMemoryFileSenderService.php
+++ src/Infrastructure/Integrations/Hosting/InMemory/InMemoryFileSenderService.php
@@ -6,13 +6,13 @@ namespace App\Infrastructure\Integrations\Hosting\InMemory;
 
 use App\Domain\Sender\Contracts\FileSenderService;
 use App\Domain\Sender\DTOs\EncodedFileData;
-use App\Domain\Sender\DTOs\FileHostingData;
+use App\Domain\Sender\DTOs\FileOnHostingData;
 
 class InMemoryFileSenderService implements FileSenderService
 {
-    public function send(EncodedFileData $encodedFile): FileHostingData
+    public function send(EncodedFileData $encodedFile): FileOnHostingData
     {
-        return new FileHostingData(
+        return new FileOnHostingData(
             fileId: '312312da',
             filename: $encodedFile->filename,
             webViewLink: sprintf('http://localhost:8080/%s.%s', $encodedFile->filename, $encodedFile->mimeType),
